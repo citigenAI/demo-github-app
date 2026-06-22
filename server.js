7df1ba393e16f1e4c4707cd403a46f62660311df
swaramedia <73296145+swaramedia@users.noreply.github.com>
2026-06-22 16:56:10 -0700

    Add files via upload
    testing github through UI

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 8000;
+const ROOT = __dirname;
+
+const MIME = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".webmanifest": "application/manifest+json",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".ico": "image/x-icon",
+};
+
+http.createServer((req, res) => {
+  let urlPath = decodeURIComponent(req.url.split("?")[0]);
+  if (urlPath === "/") urlPath = "/index.html";
+  const filePath = path.join(ROOT, urlPath);
+  if (!filePath.startsWith(ROOT)) { res.writeHead(403); return res.end(); }
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); return res.end("Not found"); }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { "Content-Type": MIME[ext] || "application/octet-stream" });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Serving on http://localhost:${PORT}`));
